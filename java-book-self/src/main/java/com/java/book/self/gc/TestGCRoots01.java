3825648856a537e46339c12789a6bac078d93324
dongzl <dzllikelsw@163.com>
2019-04-28 21:39:20 +0800

    Test GC Root

--- java-book-self/src/main/java/com/java/book/self/gc/TestGCRoots01.java
+++ java-book-self/src/main/java/com/java/book/self/gc/TestGCRoots01.java
@@ -1,7 +1,7 @@
 package com.java.book.self.gc;
 
 /**
- * GCRoots 测试：虚拟机栈（栈帧中的局部变量）中引用的对象作为GCRoots
+ * GCRoots 测试：虚拟机栈（栈帧中的局部变量）中引用的对象作为GCRoots 
  *  * -Xms1024m -Xmx1024m -Xmn512m -XX:+PrintGCDetails
  *  *
  *  * 扩展：虚拟机栈中存放了编译器可知的八种基本数据类型,对象引用,returnAddress类型（指向了一条字节码指令的地址）
