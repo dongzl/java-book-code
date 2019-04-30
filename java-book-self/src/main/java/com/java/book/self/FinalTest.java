0a877313bc2bcbcc7b971f1308530b1ae4531f58
dongzl <dzllikelsw@163.com>
2019-04-30 14:07:55 +0800

    Test final

--- java-book-self/src/main/java/com/java/book/self/FinalTest.java
+++ java-book-self/src/main/java/com/java/book/self/FinalTest.java
@@ -9,12 +9,18 @@ public final class FinalTest {
 
     private final int a = 100;
 
+    private final int b;
+
+    public FinalTest(int b) {
+        this.b = b;
+    }
+
     public final void test1() {
 
     }
 
     public static void main(String args[]) throws Exception {
-        FinalTest test = new FinalTest();
+        FinalTest test = new FinalTest(2);
         //test.a = 200; compile error
     }
 }
