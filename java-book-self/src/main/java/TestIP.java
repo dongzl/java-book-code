48c4df0d082f1be2969ddba6eceaea4b899aca37
dongzl <dzllikelsw@163.com>
2019-06-04 17:47:30 +0800

    test code

---
+++ java-book-self/src/main/java/TestIP.java
@@ -0,0 +1,14 @@
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-06-04 17:23
+ */
+public class TestIP {
+
+    public static void main(String args[]) throws Exception {
+        String a = "120.52.149.";
+        for (int i = 80; i <= 111; i++) {
+            System.out.println(a + i);
+        }
+    }
+}
