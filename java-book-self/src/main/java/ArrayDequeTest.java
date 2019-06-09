d122bb347d885b9b30d3b04f49144141e8b2a21e
dongzl <dzllikelsw@163.com>
2019-06-09 22:07:09 +0800

    ArrayDeque test

---
+++ java-book-self/src/main/java/ArrayDequeTest.java
@@ -0,0 +1,16 @@
+import java.util.ArrayDeque;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-06-09 22:05
+ */
+public class ArrayDequeTest {
+
+    public static void main(String args[]) throws Exception {
+        ArrayDeque<String> deque = new ArrayDeque();
+        deque.add("a");
+        deque.addFirst("b");
+        System.out.println(deque.toString());
+    }
+}
