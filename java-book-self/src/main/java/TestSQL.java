ad09bb2700cd06d7974b11130d18346769de0657
dongzl <dzllikelsw@163.com>
2019-06-24 12:03:21 +0800

    Test  sql

--- java-book-self/src/main/java/TestSQL.java
+++ java-book-self/src/main/java/TestSQL.java
@@ -6,9 +6,10 @@
 public class TestSQL {
 
     public static void main(String args[]) throws Exception {
-        String sql = "alter table mall_order_%s modify column order_type int(11) not null comment '订单类型';";
+        //String sql = "alter table mall_order_%s modify column order_type int(11) not null comment '订单类型';";
+        String sql = "update mall_order_%s set order_type = 180, modified = now() where order_id in (select order_id from mall_order_detail_%s where item_id between 36000000 and 38000000);";
         for (int i = 0; i <= 255; i++) {
-            System.out.println(String.format(sql, i));
+            System.out.println(String.format(sql, i, i));
         }
     }
 }
