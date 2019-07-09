b48cfe5fb41acd343e1b9823802bfc67588d5c9f
dongzl <dzllikelsw@163.com>
2019-07-09 15:52:10 +0800

    LinkedTransferQueue  Node class

---
+++ java-book-self/src/main/java/com/java/book/self/queue/Node.java
@@ -0,0 +1,85 @@
+package com.java.book.self.queue;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-07-09 15:40
+ */
+public final class Node {
+
+    final boolean isData;
+
+    volatile Object item;
+
+    volatile Node next;
+
+    volatile Thread waiter;
+
+    private static final sun.misc.Unsafe UNSAFE;
+
+    private static final long itemOffset;
+
+    private static final long nextOffset;
+
+    private static final long waiterOffset;
+
+    static {
+        try {
+            UNSAFE = sun.misc.Unsafe.getUnsafe();
+            Class<?> k = Node.class;
+            itemOffset = UNSAFE.objectFieldOffset(k.getDeclaredField("item"));
+            nextOffset = UNSAFE.objectFieldOffset(k.getDeclaredField("next"));
+            waiterOffset = UNSAFE.objectFieldOffset(k.getDeclaredField("waiter"));
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+
+    final boolean casNext(Node cmp, Node val) {
+        return UNSAFE.compareAndSwapObject(this, nextOffset, cmp, val);
+    }
+
+    final boolean casItem(Object cmp, Object val) {
+        return UNSAFE.compareAndSwapObject(this, itemOffset, cmp, val);
+    }
+
+    Node(Object item, boolean isData) {
+        UNSAFE.putObject(this, itemOffset, item); // relaxed write
+        this.isData = isData;
+    }
+
+    final void forgetNext() {
+        UNSAFE.putObject(this, nextOffset, this);
+    }
+
+    final void forgetContents() {
+        UNSAFE.putObject(this, itemOffset, this);
+        UNSAFE.putObject(this, waiterOffset, null);
+    }
+
+    final boolean isMatched() {
+        Object x = item;
+        return (x == this) || ((x == null) == isData);
+    }
+
+    final boolean isUnmatchedRequest() {
+        return !isData && item == null;
+    }
+
+    final boolean cannotPrecede(boolean haveData) {
+        boolean d = isData;
+        Object x;
+        return d != haveData && (x = item) != this && (x != null) == d;
+    }
+
+    final boolean tryMatchData() {
+        Object x = item;
+        if (x != null && x != this && casItem(x, null)) {
+            LockSupport.unpark(waiter);
+            return true;
+        }
+        return false;
+    }
+}
