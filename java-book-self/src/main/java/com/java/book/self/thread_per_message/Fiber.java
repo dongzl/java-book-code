921b3cd6bacc00cf7a4f74394e7b7c09dd4b224e
dongzl <dzllikelsw@163.com>
2019-07-31 15:14:50 +0800

    Thread-Per-Message pattern

---
+++ java-book-self/src/main/java/com/java/book/self/thread_per_message/Fiber.java
@@ -0,0 +1,13 @@
+package com.java.book.self.thread_per_message;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-07-31 15:13
+ */
+public class Fiber {
+
+    public static void schedule(Runnable target) {
+
+    }
+}
