66e109f16793d9103a0934f31194ecd0ef3a748e
dongzl <dzllikelsw@163.com>
2019-08-05 18:19:50 +0800

    Netty test

--- java-book-self/src/main/java/com/java/book/self/netty/NettyTest.java
+++ java-book-self/src/main/java/com/java/book/self/netty/NettyTest.java
@@ -2,7 +2,6 @@ package com.java.book.self.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
@@ -19,8 +18,11 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class NettyTest {
 
     public static void main(String args[]) throws Exception {
+        // 事件处理器
         final EchoServerHandler serverHandler = new EchoServerHandler();
+        //boss 线程组
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //worker 线程组
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
@@ -31,27 +33,34 @@ public class NettyTest {
                             ch.pipeline().addLast(serverHandler);
                         }
                     });
+            //bind 服务端端口
             ChannelFuture f = b.bind(9090).sync();
             f.channel().closeFuture().sync();
         } finally {
+            // 终止工作线程组
             workerGroup.shutdownGracefully();
+            // 终止 boss 线程组
             bossGroup.shutdownGracefully();
         }
     }
 
+    //socket 连接处理器
     static class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
+        // 处理读事件
         @Override
         public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
             cause.printStackTrace();
             ctx.close();
         }
-
+        
+        // 处理读完成事件
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
             ctx.write(msg);
         }
 
+        // 处理异常事件
         @Override
         public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
             ctx.flush();
