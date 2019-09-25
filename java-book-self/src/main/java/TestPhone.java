66925f8c06d7ddc2f0aa56d89005a8eaf057993a
dongzl <dzllikelsw@163.com>
2019-09-25 18:41:42 +0800

    Coding

---
+++ java-book-self/src/main/java/TestPhone.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-09-23 15:41
+ */
+public class TestPhone {
+
+    public static void main(String args[]) throws Exception {
+        Map<String, Integer> map = new HashMap<String, Integer>(2);
+//        map.put("北京", 1);
+//        map.put("上海", 2);
+//        map.put("广州", 3);
+//        map.put("深圳", 4);
+//        map.put("天津", 5);
+//        map.put("杭州", 6);
+//        map.put("重庆", 7);
+//        map.put("武汉", 8);
+//        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
+    }
+}
