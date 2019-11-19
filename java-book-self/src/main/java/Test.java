bffbaf9d332260c609ab32450a1b4aa7cb607f60
dongzl <dzllikelsw@163.com>
2019-11-19 19:47:38 +0800

    test

--- java-book-self/src/main/java/Test.java
+++ java-book-self/src/main/java/Test.java
@@ -1,8 +1,10 @@
 import com.google.common.base.Splitter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author dongzonglei
@@ -21,29 +23,48 @@ public class Test {
         return String.valueOf(c).equals(a);
     }
 
+//    public static void main(String args[]) throws Exception {
+//        String temp = ".abc.def";
+//        String[] keyArray = temp.split("\\.");
+//        String a = ".abc.def.hij";
+//        String b = ".abc.def.hijk";
+//        String c = ".abc.def.hijk.l";
+//        List<String> list = new ArrayList<>();
+//        list.add(a);
+//        list.add(b);
+//        list.add(c);
+//        for (String item : list) {
+//            String[] itemKeyArray = item.split("\\.");
+//            if (itemKeyArray.length < keyArray.length + 1) {
+//                continue;
+//            }
+//            StringBuilder builder = new StringBuilder();
+//            for (int i = 0; i < keyArray.length + 1; i++) {
+//                builder.append(itemKeyArray[i]);
+//                if (i < keyArray.length) {
+//                    builder.append(".");
+//                }
+//            }
+//            System.out.println(builder.toString());
+//        }
+//    }
+    
     public static void main(String args[]) throws Exception {
-        String temp = ".abc.def";
-        String[] keyArray = temp.split("\\.");
-        String a = ".abc.def.hij";
-        String b = ".abc.def.hijk";
-        String c = ".abc.def.hijk.l";
-        List<String> list = new ArrayList<>();
-        list.add(a);
-        list.add(b);
-        list.add(c);
-        for (String item : list) {
-            String[] itemKeyArray = item.split("\\.");
-            if (itemKeyArray.length < keyArray.length + 1) {
-                continue;
-            }
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < keyArray.length + 1; i++) {
-                builder.append(itemKeyArray[i]);
-                if (i < keyArray.length) {
-                    builder.append(".");
-                }
-            }
-            System.out.println(builder.toString());
+        Map<String, List<String>> keyAndChildrenMap = new HashMap<>();
+        String a = "a.b.c.d";
+        String parentKey = "/";
+        String shardingSphereKey = new StringBuilder("/").append(a.replace(".", "/")).toString();
+        for(int i = 1; i <= shardingSphereKey.lastIndexOf("/"); i = shardingSphereKey.indexOf("/", i) + 1) {
+//            i = shardingSphereKey.indexOf("/", i);
+            String childrenKey = shardingSphereKey.substring(0, shardingSphereKey.indexOf("/", i));
+            List<String> childrenKeys = keyAndChildrenMap.containsKey(parentKey) ? keyAndChildrenMap.get(parentKey) : new ArrayList<>();
+            childrenKeys.add(childrenKey);
+            keyAndChildrenMap.put(parentKey, childrenKeys);
+            parentKey = childrenKey;
         }
+        List<String> childrenKeys = keyAndChildrenMap.containsKey(parentKey) ? keyAndChildrenMap.get(parentKey) : new ArrayList<>();
+        childrenKeys.add(shardingSphereKey);
+        keyAndChildrenMap.put(parentKey, childrenKeys);
+        System.out.println(keyAndChildrenMap);
     }
 }
